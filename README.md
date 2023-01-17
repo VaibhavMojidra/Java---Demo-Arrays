351be81798f1b37a4661dfd314bfa93bd9290b36
VaibhavMojidra <vmojidra25@gmail.com>
2023-01-18 00:54:29 +0530

    Arrays

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java Demo Arrays
+
+[![Vaibhav Mojidra - output.png](https://raw.githubusercontent.com/VaibhavMojidra/Java---Demo-Arrays/master/output/output.png "Vaibhav Mojidra")](https://vaibhavmojidra.github.io/site/)
---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 22, 2 };
+		int a1[] = new int[12];
+		int[] a2 = new int[12];
+		int a3[];
+		a3= new int[] {1,2,2,32};
+		
+		System.out.println(a[2]);
+		System.out.println(a3[2]);
+	}
+}
